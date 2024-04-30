60d8faa766106854bbd45f43a56b921d6ba1e839
oksanakney <oksanakney@gmail.com>
2024-04-30 21:24:07 +0300

    Custom Model Validation: IsBefore Validation Attribute Added

---
+++ ProjectWithUtilities/Attributes/IsBefore.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace AspNetCoreUtilities.Attributes
+{
+    public class IsBefore : ValidationAttribute // not an interface, has error message inside
+    {
+        private const string DateTimeFormat = "dd.MM.yyyy";
+        private readonly DateTime date;
+
+        // Se polzavat edno kam edno kato vgradeni validation attributes
+        public IsBefore(string dateInput)
+        {
+            date = DateTime.ParseExact(dateInput, DateTimeFormat, CultureInfo.InvariantCulture);
+        }
+
+        public string DatePurpose { get; set; }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null && (DateTime)value >= date)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ProjectWithUtilities/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreUtilities.Attributes;
+
+namespace AspNetCoreUtilities.Models
+{
+    public class HomeViewModel
+    {
+        // ctor of attribite becomes this, every public prop can be written like DatePurpose and it takes its value
+        [IsBefore("30.04.2024", ErrorMessage = "Date must be before 30.04.2024", DatePurpose = "Hello")]
+        public DateTime MyDate { get; set; }
+    }
+}
