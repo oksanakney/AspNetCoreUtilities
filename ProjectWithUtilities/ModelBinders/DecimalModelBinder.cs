9e7987e3eadc82306d6e0da2f0de1de5498da612
oksanakney <oksanakney@gmail.com>
2024-04-30 20:07:26 +0300

    Add project files.

---
+++ ProjectWithUtilities/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace AspNetCoreUtilities.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)// framework si go palni, kogato dojde rewuest
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            // praznata stojnost, prazen string, ne e zadalz proverka, v http prot vsi4ko e string
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal result = 0M; // defalt value
+                bool success = false;
+
+                try
+                {
+                    string strValue = valueResult.FirstValue.Trim();
+                    strValue = strValue.Replace(".", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+                    strValue = strValue.Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = Convert.ToDecimal(strValue, CultureInfo.CurrentCulture);
+                    success = true;
+                }
+                // samo fe, vseki drugi e ne iskam da obrabotvam
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (success) 
+                { 
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ProjectWithUtilities/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNetCoreUtilities.ModelBinders
+{
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        // Providera predi niamawe, sega ima, zawtoto kazva kakyv tip propertita triabva da vikneme
+        // Tova go lipsvawe v prediwni versii
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context);
+            }
+
+            if (context.Metadata.ModelType == typeof(decimal) 
+                || context.Metadata.ModelType == typeof(decimal?))
+            {
+                return new DecimalModelBinder();
+            }
+
+            return null; // ako varne null we byde ignoriran i we se varne kam sledvawia
+        }
+    }
+}
