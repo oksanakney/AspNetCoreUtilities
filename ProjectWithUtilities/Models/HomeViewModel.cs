594d97b19e2a462ca0a32acfc6cb34ed7ff01575
oksanakney <oksanakney@gmail.com>
2024-04-30 21:53:55 +0300

    IValidatableObject, which implements IEnumerable Added in order to work with Complex Dependency

--- ProjectWithUtilities/Models/HomeViewModel.cs
+++ ProjectWithUtilities/Models/HomeViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using AspNetCoreUtilities.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetCoreUtilities.Models
 {
-    public class HomeViewModel
+    public class HomeViewModel : IValidatableObject // slozhna zavisimost, v otlichie ot : ValidationAttribute ne e preizpolzvaem
     {
         // ctor of attribite becomes this, every public prop can be written like DatePurpose and it takes its value
         [IsBefore("30.04.2024", ErrorMessage = "Date must be before 30.04.2024", DatePurpose = "Hello")]
@@ -10,5 +11,27 @@ namespace AspNetCoreUtilities.Models
 
         [IsAdult(18, ErrorMessage = "Must be at least 18 years old")]
         public DateTime Birthday { get; set; }
+
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required");
+            }
+
+            if (string.IsNullOrEmpty(Country))
+            {
+                yield return new ValidationResult("Name is required");
+            }
+
+            // Slozhna zavisimost
+            if (Name == "Pesho" && Country != "BG")
+            {
+                yield return new ValidationResult("If name is Pesho, Country must be BG");
+            }
+        }
     }
 }
