6260a63ea56cf7d851d383f52454eabbdcf132ec
oksanakney <oksanakney@gmail.com>
2024-04-30 21:36:47 +0300

    Custom Model Validation: Validation Attribute Is Adult Added

---
+++ ProjectWithUtilities/Attributes/IsAdult.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreUtilities.Attributes
+{
+    public class IsAdult : ValidationAttribute
+    {
+        private readonly DateTime minimunAge = DateTime.Today.AddYears(-18);
+        private readonly int adultAge = 18;
+
+        public IsAdult(int age)
+        {
+            adultAge = age;
+            minimunAge = DateTime.Today.AddYears(age * -1);
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null && (DateTime)value <= minimunAge) 
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
--- ProjectWithUtilities/Models/HomeViewModel.cs
+++ ProjectWithUtilities/Models/HomeViewModel.cs
@@ -7,5 +7,8 @@ namespace AspNetCoreUtilities.Models
         // ctor of attribite becomes this, every public prop can be written like DatePurpose and it takes its value
         [IsBefore("30.04.2024", ErrorMessage = "Date must be before 30.04.2024", DatePurpose = "Hello")]
         public DateTime MyDate { get; set; }
+
+        [IsAdult(18, ErrorMessage = "Must be at least 18 years old")]
+        public DateTime Birthday { get; set; }
     }
 }
